d2c4b29d6da4c9ced5d42d6091495df849b88173
wycc <wychao_1016@sina.com>
2018-06-14 13:58:53 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/js/')
+  },
+ module: {
+   rules: [
+     {
+       test: /\.js$/,
+       exclude: /(node_modules|bower_components)/,
+       use: {
+         loader: 'babel-loader',
+         options: {
+           presets: ['env']
+         }
+       }
+     },
+     {
+       test: /\.scss$/,
+       use: [{
+       loader: "style-loader"
+            }, {
+              loader: "css-loader"
+            }, {
+              loader: "sass-loader",
+            }]
+         }
+     ]
+   }
+};
